2204cd48f9052003a0978c503fc8ace85ca5ddfd
gLombar4 <lombard.garrett@gmail.com>
2025-03-11 17:00:11 -0400

    added fonts, icons, & a bit of content

---
+++ itis3135/hobby/script.js
@@ -0,0 +1,27 @@
+const personalSection = document.getElementById("personal-experience");
+const whatSection = document.getElementById("what");
+const timelineSection = document.getElementById("timeline");
+const platformsSection = document.getElementById("platforms");
+const howSection = document.getElementById("how");
+const whySection = document.getElementById("why");
+const sections = [personalSection, whatSection, timelineSection, platformsSection, howSection, whySection];
+
+function hideSections(sectionId) {
+    const currentSection = document.getElementById(sectionId);
+    sections.forEach(section => {
+        if (section !== currentSection) {
+            section.style.display = "none";
+        }
+    })
+    currentSection.style.display = "block";
+}
+
+function onLoad() {
+    const currentSection = document.getElementById("what");
+    sections.forEach(section => {
+        if (section !== currentSection) {
+            section.style.display = "none";
+        }
+    })
+    currentSection.style.display = "block";
+}
