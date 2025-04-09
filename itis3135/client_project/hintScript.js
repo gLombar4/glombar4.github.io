66676155ccfa02732d29409bc381bf8650319a53
gLombar4 <lombard.garrett@gmail.com>
2025-04-09 12:37:02 -0400

    added "getting started" section to client project

---
+++ itis3135/client_project/hintScript.js
@@ -0,0 +1,9 @@
+function showHint(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+    }
+    
+}
